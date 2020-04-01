7938892c79713f41c3435c63b935013c9c371cfe
yysaki <mail@yysaki.com>
2020-04-01 20:52:36 +0900

    Add s3

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
+# chapter 3
+
 module "web_server" {
   source        = "./http_server"
   instance_type = "t3.micro"
 }
+
+# chapter 5
 
 data "aws_iam_policy_document" "allow_describe_regions" {
   statement {
@@ -16,6 +20,74 @@ module "describe_regions_for_ec2" {
   name       = "describe-regions-for-ec2"
   identifier = "ec2.amazonaws.com"
   policy     = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+resource "aws_s3_bucket" "private" {
+  bucket = "private-pragmatic-terraform-study"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# chapter 6
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform-study"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-progmatic-terraform-public"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
 }
 
 provider "aws" {
