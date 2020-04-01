0d2a46b78c0eb0c216070bd2cc71ea2e655b1843
yysaki <mail@yysaki.com>
2020-04-01 20:20:54 +0900

    Add iam_role module

--- main.tf
+++ main.tf
@@ -3,6 +3,21 @@ module "web_server" {
   instance_type = "t3.micro"
 }
 
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
