4d47ba3914d2e8f50d88b1ae44ca332c2343ed29
yysaki <mail@yysaki.com>
2020-04-09 22:36:00 +0900

    Add route53

--- main.tf
+++ main.tf
@@ -287,3 +287,23 @@ resource "aws_lb_listener" "http" {
     }
   }
 }
+
+data "aws_route53_zone" "example" {
+  name = "example.com"
+}
+
+resource "aws_route53_record" "example" {
+  zone_id = data.aws_route53_zone.example.zone_id
+  name    = data.aws_route53_zone.example.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.example.dns_name
+    zone_id                = aws_lb.example.zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "domain_name" {
+  value = aws_route53_record.example.name
+}
