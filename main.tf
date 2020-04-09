582533c827a2c7373d2f4a08afc36104236ec1ce
yysaki <mail@yysaki.com>
2020-04-09 22:41:48 +0900

    Add ACM certificate

--- main.tf
+++ main.tf
@@ -307,3 +307,26 @@ resource "aws_route53_record" "example" {
 output "domain_name" {
   value = aws_route53_record.example.name
 }
+
+resource "aws_acm_certificate" "example" {
+  domain_name               = aws_route53_record.example.name
+  subject_alternative_names = []
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "example_certificate" {
+  name    = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+  records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+  zone_id = data.aws_route53_zone.example.id
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.example.arn
+  validation_record_fqdns = [aws_route53_record.example_certificate.fqdn]
+}
