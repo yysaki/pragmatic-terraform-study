78a8b589d1501f74fa6c16d1f676bd00e2969b19
yysaki <mail@yysaki.com>
2020-04-12 16:38:21 +0900

    Add codebuild

--- main.tf
+++ main.tf
@@ -690,24 +690,169 @@ resource "aws_ecr_repository" "example" {
 
 resource "aws_ecr_lifecycle_policy" "example" {
   repository = aws_ecr_repository.example.name
+  policy     = file("./ecr_lifecycle_policy.json")
+}
 
-  policy = <<EOF
-  {
-    "rules": [
-      {
-        "rulePriority": 1,
-        "description": "Keep last 30 relaease tagged images",
-        "selection": {
-          "tagStatus": "tagged",
-          "tagPrefixList": ["release"],
-          "countType": "imageCountMoreThan",
-          "countNumber": 30
-        },
-        "action": {
-          "type": "expire"
-        }
-      }
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
     ]
   }
-EOF
+}
+
+module "codebuild_role" {
+  source     = "./iam_role"
+  name       = "codebuild"
+  identifier = "codebuild.amazonaws.com"
+  policy     = data.aws_iam_policy_document.codebuild.json
+}
+
+resource "aws_codebuild_project" "example" {
+  name         = "example"
+  service_role = module.codebuild_role.iam_role_arn
+
+  source {
+    type = "CODEPIPELINE"
+  }
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type            = "LINUX_CONTAINER"
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:2.0"
+    privileged_mode = true
+  }
+}
+
+data "aws_iam_policy_document" "codepipeline" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+      "ecs:DescribeServices",
+      "ecs:DescribeTaskDefinition",
+      "ecs:DescribeTasks",
+      "ecs:ListTasks",
+      "ecs:RegisterTaskDefinition",
+      "ecs:UpdateService",
+      "iam:PassRole",
+    ]
+  }
+}
+
+module "codepipeline_role" {
+  source     = "./iam_role"
+  name       = "codepipeline"
+  identifier = "codepipeline.amazonaws.com"
+  policy     = data.aws_iam_policy_document.codepipeline.json
+}
+
+resource "aws_s3_bucket" "artifact" {
+  bucket = "artifact-pragmatic-terraform-study"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_codepipeline" "example" {
+  name     = "example"
+  role_arn = module.codepipeline_role.iam_role_arn
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = 1
+      output_artifacts = ["Source"]
+
+      configuration = {
+        Owner                = "yysaki"
+        Repo                 = "pragmatic-terraform-study"
+        Branch               = "master"
+        PollForSourceChanges = false
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = 1
+      input_artifacts  = ["Source"]
+      output_artifacts = ["Build"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.example.id
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "ECS"
+      version         = 1
+      input_artifacts = ["Build"]
+
+      configuration = {
+        ClusterName = aws_ecs_cluster.example.name
+        ServiceName = aws_ecs_service.example.name
+        FileName    = "imagedefinitions.json"
+      }
+    }
+  }
+
+  artifact_store {
+    location = aws_s3_bucket.artifact.id
+    type     = "S3"
+  }
 }
